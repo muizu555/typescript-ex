7e5303f8b32226343c274ed18605f5d8b2b008df
muizu <muizu0920555@protonmail.com>
2023-07-06 00:19:08 +0900

    logic

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -1,7 +1,14 @@
 import express, { RequestHandler} from 'express';
 
-const TODOS = [];
+import { Todo } from "../models/todo";
+
+const TODOS: Todo[] = [];
 
 export const createTodo: RequestHandler = (req, res, next) => {
-    const newTodo = {};
+    const text = (req.body as {text: string}).text;
+    const newTodo = new Todo(Math.random().toString(), text);
+
+    TODOS.push(newTodo);//メモリ上にのみ保存
+
+    res.status(201).json({message: "リスト作成", createTodo: newTodo});
 }
