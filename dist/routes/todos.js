d0a79823351c7f3ea3f431c1dbd0fc616a6992d8
muizu <muizu0920555@protonmail.com>
2023-07-06 13:49:45 +0900

    fin

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -4,8 +4,8 @@ const express_1 = require("express");
 const todos_1 = require("../controllers/todos");
 const router = (0, express_1.Router)();
 router.post("/", todos_1.createTodo);
-router.get("/");
-router.patch("/:id");
-router.delete("/:id");
+router.get("/", todos_1.getTodos);
+router.patch("/:id", todos_1.updateTodo);
+router.delete("/:id", todos_1.deleteTodo);
 exports.default = router;
 //# sourceMappingURL=todos.js.map
--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -12,3 +12,42 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
     res.status(201).json({message: "リスト作成", createTodo: newTodo});
 }
+
+export const getTodos: RequestHandler = (req, res, next) => {
+   res.json({todos: TODOS})//todosのkeyでTODOSを返した
+}
+
+export const updateTodo: RequestHandler<{id: string}> = (req, res, next) => {
+    const todoId = req.params.id;
+    const updateText = (req.body as {text: string}).text;
+
+
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    
+    if(todoIndex < 0){
+        throw new Error("Not find TODO");
+    }
+
+    TODOS[todoIndex] = new Todo(todoId, updateText);
+
+    res.json({message: "update", updatedTodo: TODOS[todoIndex]});
+}
+
+export const deleteTodo: RequestHandler<{id: string}> = (req, res, next) => {
+    const todoId = req.params.id;
+    
+
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    
+    if(todoIndex < 0){
+        throw new Error("Not find TODO");
+    }
+
+    TODOS.splice(todoIndex, 1);
+
+    res.json({message: "delete todo"});
+}
+
+
+
+
